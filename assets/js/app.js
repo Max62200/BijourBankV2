e4334368102199be89ebe239877bba643de6e544
Max62200 <podevinmaxence@gmail.com>
2021-10-26 15:44:01 +0200

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,104 @@
+console.log('Bijour Bank !');
+/**
+ * init foundation
+ */
+$(document).ready(function() {
+	$(document).foundation();
+});
+
+//add to local storage
+
+const operationForm = document.getElementById('operationForm');
+operationForm.addEventListener('submit', () => {
+	let i = localStorage.length;
+	let arrayStorage = {
+		operator: document.getElementById('operator').value,
+		titre: document.getElementById('titre').value,
+		desc: document.getElementById('desc').value,
+		montant: document.getElementById('montant').value,
+	};
+	localStorage.setItem(i, JSON.stringify(arrayStorage));
+});
+
+// initialisation des variables
+
+let valueCredit = 0;
+let valueDebit = 0;
+var total = 0;
+var solde = document.getElementById('solde');
+solde.innerHTML = total + `€`;
+
+//loop for display element of this array
+for (let i = 0; i < localStorage.length; i++) {
+	let obj = JSON.parse(localStorage.getItem(i));
+
+	// condition for sold
+	var img = '';
+	if (obj.operator == 'credit') {
+		var img = 'sac-dargent';
+		valueCredit = valueCredit + Number(obj.montant);
+	} else {
+		var img = 'depenses';
+		valueDebit = valueDebit + Number(obj.montant);
+	}
+
+	// calcule du solde
+
+	total = valueCredit - valueDebit;
+	var solde = document.getElementById('solde');
+	solde.innerHTML = total + `€`;
+
+	// calcule taux variation
+	for (let i = 0; i < localStorage.length; i++) {
+		let obj = JSON.parse(localStorage.getItem(i));
+
+		var a = total - obj.montant;
+		var b = total;
+
+		var taux = (((b - a) / a) * 100).toFixed(2);
+	}
+
+	// code html injected
+
+	var codeHtml = `    
+    <div class="grid-container">
+    <div class="operation ${obj.operator}">
+      <div class="grid-x grid-padding-x align-middle">
+      <div class="cell shrink">
+      <div class="picto">
+        <img src="./assets/images/${img}.png" alt="" />
+      </div>
+    </div>
+        <div class="cell auto">
+          <div>
+            <h2>${obj.titre}</h2>
+            <small>${obj.desc}</small>
+          </div>
+        </div>
+        <div class="cell small-3 text-right">
+          <div>
+            <p class="count">${obj.montant}€</p>
+            <small>${taux}%</small>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+    `;
+	// replace html
+
+	var replace = document.getElementById('grid-container');
+	replace.insertAdjacentHTML('afterbegin', codeHtml);
+}
+
+// condition good / bad color class
+
+var state = document.querySelector('#solde + small');
+
+if (total >= 0) {
+	state.setAttribute('class', 'good');
+	state.innerText = 'on est good 👍';
+} else {
+	state.setAttribute('class', 'bad');
+	state.innerText = ' on est dans la merde 👎';
+}
